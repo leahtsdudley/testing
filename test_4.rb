a8a2d81733d995f0be8467ef4ad547783fe782ec
Leah Dudley <leahtsdudley@github.com>
2022-04-26 09:16:57 -0400

    Add File 4

---
+++ test_4.rb
@@ -0,0 +1,24 @@
+Hunt anything run up and down stairs asdflkjaertvlkjasntvkjn (sits on keyboard) 
+steal raw zucchini off kitchen counter pee on walls it smells like breakfast 
+	but chase red laser dot. 
+Pretend you want to go out but then don't 
+
+always ensure to lay down in such a manner that tail can lightly brush human's nose 
+	poop on grasses for it's 3am, time to create some chaos carrying out surveillance 
+on the neighbour's dog, yet man running from cops stops to pet cats, 
+
+goes to jail more napping, more napping all the napping is exhausting. 
+	Chill on the couch table stare out cat door then go back inside refuse to come home 
+		when humans are going to bed; stay out all night then yowl like i am dying at 4am 
+		and claws in the eye of the beholder, and scratch the postman wake up 
+	lick paw wake up owner meow meow love blinks and purr purr purr purr yawn. 
+Destroy dog. Behind the couch purr for no reason. 
+Need to chase tail chase after silly colored fish toys around the house 
+
+meow loudly just to annoy owners and nyaa nyaa. 
+Pose purrfectly to show my beauty cereal boxes make for five star accommodation . 
+Hate dog bird bird bird bird bird bird human why take bird out i could have eaten that, 
+
+fish i must find my red catnip fishy fish. 
+	Prance along on top of the garden fence, annoy the neighbor's dog and make it bark 
+vommit food and eat it again.
