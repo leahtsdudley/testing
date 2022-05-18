916e60c717f5a1f2fbd8d327ddee31c7cd83abc2
Leah Dudley <leahtsdudley@github.com>
2022-05-18 14:43:55 -0400

    Add files via upload

---
+++ test_3.rb
@@ -0,0 +1,26 @@
+Sweet beast sniff catnip and act crazy relentlessly pursues moth 
+
+cattt catt cattty cat being a cat but run around the house at 4 in the morning 
+	so a nice warm laptop for me to sit on. 
+Eat fish on floor meoooow cat walks in keyboard furball roll roll roll rub my belly hiss. 
+
+Lick human with sandpaper tongue trip owner up in kitchen i want food. 
+
+Step on your keyboard while you're gaming and then turn in a circle ignore the squirrels, 
+	you'll never catch them anyway. 
+Fooled again thinking the dog likes me jump on human and sleep on her all night 
+
+long be long in the bed, purr in the morning and then give a bite to every human around 
+	for not waking up request food, purr loud scratch the walls, the floor, the windows, the humans 
+
+	so naughty running cat for sit by the fire cat jumps and falls onto the couch 
+	purrs and wakes up in a new dimension filled with kitty litter 
+
+meow meow yummy there is a bunch of cats hanging around eating catnip . 
+
+A nice warm laptop for me to sit on jump launch to pounce upon little yarn mouse, 
+	bare fangs at toy run hide in litter box until treats are fed so 
+freak human out make funny noise mow mow mow mow mow mow success 
+
+	now attack human fish i must find my red catnip fishy fish whatever 
+throw down all the stuff in the kitchen.
