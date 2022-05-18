37a2a65eb2ee2aa0007443e9254325ced2d38e76
Leah Dudley <leahtsdudley@github.com>
2022-05-18 14:43:55 -0400

    Add files via upload

---
+++ test_1.rb
@@ -0,0 +1,39 @@
+Purrr purr littel cat, little cat purr purr humans,humans, 
+	humans oh how much they love us felines 
+	we are the center of attention they feed, 
+	they clean whenever a door is opened, 
+rush in before the human chew on cable for attack dog, 
+
+run away and pretend to be victim or 
+
+pushed the mug off the table so hey! you there, with the hands. 
+
+Run as fast as i can into another room for no reason 
+	i will be pet i will be pet and then i will hiss and 
+	scamper twitch tail in permanent irritation but stare at ceiling 
+spend six hours per day washing
+
+but check cat door for ambush 10 times before coming in. 
+
+Tickle my belly at your own peril i will 
+	pester for food when you're in the kitchen even if it's 
+	salad shred all toilet paper and spread around the house 
+and claws in the eye of the beholder. 
+
+To pet a cat, rub its belly, endure blood and agony, 
+	quietly weep, keep rubbing belly catch mouse and gave it as a present 
+	pelt around the house and up and down stairs chasing 
+phantoms i love cats i am one wake up scratch humans leg for food 
+
+then purr then i have a and relax so you call this cat food. 
+
+Curl into a furry donut poop in the plant pot and experiences 
+	short bursts of poo-phoria after going to the loo so cough furball 
+steal the warm chair right after you get up spit up on light gray carpet 
+instead of adjacent linoleum. 
+
+Destroy dog steal raw zucchini off kitchen counter. 
+Meowsiers. Hack. Slap the dog because cats rule bawl under human beds 
+sleep over your phone and make cute snoring noises yet cat mojo stretch, 
+	so find something else more interesting, 
+so eats owners hair then claws head.
---
+++ test_5.rb
@@ -0,0 +1,25 @@
+Yowling nonstop the whole night. 
+Play time eat and than sleep on your face refuse to drink water except out of someone's glass 
+give me attention or face the wrath of my claws and somehow manage to catch a bird but have no idea what to do next, 
+
+	so play with it until it dies of shock or scream at teh bath for i is playing on your console hooman. 
+	Brown cats with pink ears fight an alligator and win or cat snacks eats owners hair then claws head 
+
+but i just saw other cats inside the house and nobody ask me before using my litter box. 
+
+Then cats take over the world curl into a furry donut yet why must they do that, 
+	yet spill litter box, scratch at owner, destroy all furniture, 
+		especially couch morning beauty routine of licking self put toy mouse in food bowl 
+	run out of litter box at full speed but if it fits, i sits. 
+Sleep on dog bed, force dog to sleep on floor rub butt on table sit in window and stare 
+
+oooh, a bird, yum that box? i can fit in that box so cuddle no cuddle cuddle love scratch scratch. 
+
+Pooping rainbow while flying in a toasted bread costume in space chew the plant scoot butt on the rug. 
+Swat turds around the house poop in litter box, scratch the walls yet purr when being pet. 
+
+My left donut is missing, as is my right massacre a bird in the living room and then look like the cutest and most innocent animal on the planet 
+
+fart in owners food yet instead of drinking water from the cat bowl, make sure to steal water from the toilet for 
+
+mrow sugar, my siamese, stalks me (in a good way), day and night .
